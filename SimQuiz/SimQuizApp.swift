59a0ba8efc19b6b31ed5ed6c1ce480332cb6abd9
Ardak <ardaktursunbayev4@gmail.com>
2024-04-24 15:28:39 +0600

    Initial Commit

---
+++ SimQuiz/SimQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimQuizApp.swift
+//  SimQuiz
+//
+//  Created by Ardak Tursunbayev on 24.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SimQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
